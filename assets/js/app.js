fdab34db19c8b2bc3a2471526bcd10bc234fa6d9
anamerasul <anamerasul@gmail.com>
2022-03-02 19:35:08 +0600

    Display temperature, city, dynamic weather icon

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,20 +8,34 @@ const searchTempareture= async()=>{
        const cityValue=city.value;
        console.log(cityValue);
 
-       const url =`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${API_KEY}`
+       const url =`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${API_KEY}&units=metric`
 
        console.log(url);
 // const url_two= await url
-       fetch(url)
-       .then(res=>res.json())
-       .then(data =>displayTempareture(data))
+//        fetch(url)
+//        .then(res=>res.json())
+//        .then(data =>displayTempareture(data))
 
-// const res =await fetch(url);
-// const data= await res.json();
-// console.log(data)
+const res =await fetch(url);
+const data= await res.json();
+const load = displayTempareture(data)
+
+city.value=``;
 }
 
+const setInnerText=(id,text)=>{
+        document.getElementById(id).innerText=text
+}
 
 const displayTempareture=tempareture=>{
-console.log(tempareture)
+console.log(tempareture);
+setInnerText('city',tempareture.name)
+setInnerText('temp',tempareture.main.temp)
+setInnerText('temp-condition',tempareture.weather[0].main);
+
+//set weather icon
+const url =`http://openweathermap.org/img/wn/${tempareture.weather[0].icon}@2x.png`
+
+const imgIcon=document.getElementById('w-icon');
+imgIcon.setAttribute('src',url)
 }
